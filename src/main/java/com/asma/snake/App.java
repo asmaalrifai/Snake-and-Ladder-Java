147ab350cd1c862c43004bd13e4076a5567b07e7
Asma Alrifai <asma.o.alrifai@gmail.com>
2025-04-22 14:29:57 +0300

    JavaFX working setup + basic App UI

--- src/main/java/com/asma/snake/App.java
+++ src/main/java/com/asma/snake/App.java
@@ -1,13 +1,25 @@
 package com.asma.snake;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("🎲 Snake and Ladder - JavaFX Works!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setTitle("Snake and Ladder Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
