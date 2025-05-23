d88b26518f5f96460014da4e2c2a1ae9fecc5a62
asmaalrifai <asma.o.alrifai@gmail.com>
2025-05-23 18:16:46 +0300

    Adjust server port and host

--- src/main/java/com/asma/snake/client/GameUI.java
+++ src/main/java/com/asma/snake/client/GameUI.java
@@ -32,6 +32,7 @@ public class GameUI {
 
     // Server address and port (modifiable from one place)
     private static final String SERVER_HOST = "localhost";
+    // private static final String SERVER_HOST = "16.171.141.21";
     private static final int SERVER_PORT = 40000;
 
     // Player tokens and board display
